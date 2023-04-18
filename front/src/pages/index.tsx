e81fa173ac1d41bb2b198fccfa80d1bb8f8abbdd
ArtJSON <artur.pietrzak1@outlook.com>
2023-04-18 23:45:41 +0200

    Create usePostsHook

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
 import { useSession } from "../main";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.0.1:8080/";
 
 export default function IndexPage() {
+  const { posts, hasNext, fetchMore } = usePostsHook();
+  console.log(posts);
+
   return (
     <div className="page">
       <h1>{"null"}</h1>
       <BottomBar />
+      <button
+        onClick={() => {
+          fetchMore();
+        }}
+      >
+        dasdsa
+      </button>
     </div>
   );
 }
@@ -29,4 +43,31 @@ const BottomBar = () => {
       )}
     </div>
   );
+};
+interface Post {
+  id: String;
+  username: String;
+  content: String;
+}
+
+const usePostsHook = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState(0);
+  const [hasNext, setHasNext] = useState(true);
+
+  const fetchMore = () => {
+    if (hasNext) {
+      axios
+        .put("/post", {
+          page: page,
+        })
+        .then((res) => {
+          setPage((prev) => prev + 1);
+          setPosts((prev) => [...prev, ...res.data.posts]);
+          setHasNext(res.data.has_next);
+        });
+    }
+  };
+
+  return { posts, hasNext, fetchMore };
 };
