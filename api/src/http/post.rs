d64bed5fb16bc21dfa78d595ac587d35de08a7b6
ArtJSON <artur.pietrzak1@outlook.com>
2023-04-18 20:58:16 +0200

    Add paginated get_posts

--- api/src/http/post.rs
+++ api/src/http/post.rs
@@ -1,5 +1,70 @@
-use axum::Router;
+use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+
+use super::PoolConnection;
 
 pub fn router(state_pool: super::PoolConnection) -> Router {
-    Router::new().with_state(state_pool)
+    Router::new()
+        .route("/", get(get_posts))
+        .with_state(state_pool)
+}
+
+#[derive(Serialize)]
+struct Post {
+    id: String,
+    username: String,
+    content: String,
+}
+
+#[derive(Deserialize)]
+struct GetPostsPayload {
+    page: Option<u32>,
+}
+
+#[derive(Serialize)]
+struct GetPostsResponse {
+    has_next: bool,
+    posts: Vec<Post>,
+}
+
+async fn get_posts(
+    State(pool): State<PoolConnection>,
+    payload: Json<GetPostsPayload>,
+) -> impl IntoResponse {
+    let offset = payload.page.unwrap_or(0) * 25;
+
+    let row = sqlx::query!(
+        "
+        SELECT post_tbl.id, post_tbl.content, user_tbl.username
+        FROM post_tbl
+        LEFT JOIN user_tbl
+        ON post_tbl.user_id = user_tbl.id
+        LIMIT ?,10
+        ",
+        offset
+    )
+    .fetch_all(&(*pool))
+    .await
+    .unwrap();
+
+    let count = sqlx::query!(
+        "
+        SELECT COUNT(*) as count FROM post_tbl
+        "
+    )
+    .fetch_one(&(*pool))
+    .await
+    .unwrap();
+
+    Json(GetPostsResponse {
+        has_next: count.count > offset.into(),
+        posts: row
+            .iter()
+            .map(|record| Post {
+                content: record.content.clone().unwrap_or(String::from("")),
+                id: record.id.clone(),
+                username: record.username.clone().unwrap_or(String::from("")),
+            })
+            .collect(),
+    })
 }
